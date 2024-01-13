fe49a0fa87396e54da675442e5ad15a672e93249
Marvin Martins <marvin.mseq@gmail.com>
2024-01-13 15:04:31 -0300

    Salvando os dados de usuário do QRcode do whatsapp web.

--- bot.py
+++ bot.py
@@ -1,6 +1,12 @@
 from selenium import webdriver
 import time
+import os
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome()
+dir_path = os.getcwd()
+chrome_options2 = Options()
+chrome_options2.add_argument(r"user-data-dir=" + dir_path + "profile/zap")
+
+driver = webdriver.Chrome(chrome_options2)
 driver.get('https://web.whatsapp.com/')
 time.sleep(60)
